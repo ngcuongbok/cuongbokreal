8fa74cf65bd39ca1c9d2699e786b8e8e5ed6e117
ngcuongbok <82893868+ngcuongbok@users.noreply.github.com>
2021-09-12 12:57:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -68,22 +68,25 @@ var jsonData = [
 
 let data = "";
 for(let i=0; i<jsonData.length; i++){
-  data += '<a itemprop="url" target="_blank" href="'+ jsonData[i].linksp+'" class="eco-catalog-product-desktop-vertical-simple  block rounded-lg bg-white shadow-custom-1  product-hover-animation overflow-hidden h-full flex flex-col relative" title=""><div class="relative h-0 " style="padding-bottom: 100%;"><div class="absolute top-0 left-0 right-0 bottom-0 w-full h-full"><div class="w-full h-full"><div class="lazyload-wrapper"><img class="w-full h-full object-contain" src="'+jsonData[i].imgUrl+'" title="'+jsonData[i].title+'" alt="'+jsonData[i].title+'" style="object-fit: cover;"/></div></div></div></div><div class="py-4 px-3 flex-grow flex flex-col "><div class="text-xs md:text-base flex-grow"><span class="" width="0"><span><span>'+jsonData[i].title+'</span></span><span style="position: fixed; visibility: hidden; top: 0px; left: 0px;">...</span></span></div></div></a>';
+  data +=  `<a itemprop="url" target="_blank" href="${jsonData[i].linksp}" class="eco-catalog-product-desktop-vertical-simple  block rounded-lg bg-white shadow-custom-1  product-hover-animation overflow-hidden h-full flex flex-col relative" title="${jsonData[i].title}">
+  <div class="relative h-0 " style="padding-bottom: 100%;">
+  <div class="absolute top-0 left-0 right-0 bottom-0 w-full h-full">
+  <div class="w-full h-full"><div class="lazyload-wrapper">
+  <img class="w-full h-full object-contain" src="${jsonData[i].imgUrl}" title="${jsonData[i].title}" alt="${jsonData[i].title}" style="object-fit: cover;"/></div></div></div></div>
+  <div class="py-4 px-3 flex-grow flex flex-col "><div class="text-xs md:text-base flex-grow"><span class="" width="0">
+  <span><span>${jsonData[i].title}</span></span><span style="position: fixed; visibility: hidden; top: 0px; left: 0px;">...</span></span></div></div></a>`;
 }
 document.getElementById("innerdata").innerHTML = data;
 
-function copyLink() {
-    
+function copyLink() { 
     toast({
       title: "Thành công!",
       message: "Đã sao chép liên kết",
       type: "success",
       duration: 3500
     });
-    
   /* Get the text field */
   var copyTextLink = document.getElementById("textlink");
-
   /* Select the text field */
   copyTextLink.select();
   copyTextLink.setSelectionRange(0, 99999); /* For mobile devices */
@@ -95,8 +98,6 @@ function copyLink() {
   document.getElementById("thongbaocopy").style.display = "block";
   setTimeout(function(){ document.getElementById("thongbaocopy").style.display = "none"; }, 2000);
 }
-
-
     // Toast function
 function toast({ title = "", message = "", type = "info", duration = 3000 }) {
   const main = document.getElementById("toast");
