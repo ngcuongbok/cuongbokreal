53eaa08f2d1e192a54847723c82266af99baaa21
ngcuongbok <82893868+ngcuongbok@users.noreply.github.com>
2021-09-20 18:39:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -36,6 +36,10 @@ for(let a=0; a<ghimData.length; a++){
 };
 
 var jsonData = [
+    {"title"  : "[SHOPEE] Mã Giảm Giá Trung Thu",
+     "imgUrl" : "https://i.imgur.com/7jCHVSa.jpg",
+     "linksp" : "https://cuongbokit.blogspot.com/2021/09/shopee-pha-co-trung-thu-deal-tu-1k.html?utm_source=cuongbokrealbio"
+    }, 
     {"title"  : "Code hack đáp án Friends 2021",
      "imgUrl" : "https://i.imgur.com/ZVbQztG.jpg",
      "linksp" : "https://cuongbokit.blogspot.com/2021/09/share-code-hack-dap-an-thu-thach-ban-be-friends-2021.html?utm_source=cuongbokrealbio"
